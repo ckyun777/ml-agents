994dd0d87e13fe6a59c3aca91deced7d3f83d850
Yunkyu Choi <ckyun777@gmail.com>
2018-12-10 05:09:40 +0900

    bandit

---
+++ UnitySDK/Assets/05_MyMLTest/2_Scripts/Simple/Bandit.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bandit : MonoBehaviour
+{
+	public Material Gold;
+	public Material Silver;
+	public Material Bronze;
+	private MeshRenderer mesh;
+	private Material reset;
+
+	// Use this for initialization
+	void Start()
+	{
+		mesh = GetComponent<MeshRenderer>();
+		reset = mesh.material;
+	}
+
+	public int PullArm(int arm)
+	{
+		var reward = 0;
+		switch (arm)
+		{
+			case 1:
+				mesh.material = Gold;
+				reward = 3;
+				break;
+			case 2:
+				mesh.material = Bronze;
+				reward = 1;
+				break;
+			case 3:
+				mesh.material = Bronze;
+				reward = 1;
+				break;
+			case 4:
+				mesh.material = Silver;
+				reward = 2;
+				break;
+		}
+		return reward;
+	}
+
+	public void Reset()
+	{
+		mesh.material = reset;
+	}
+}
---
+++ UnitySDK/Assets/05_MyMLTest/2_Scripts/Simple/SimpleAgent.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class SimpleAgent : Agent {
+
+	public Bandit bandit;
+
+	public override void AgentAction(float[] vectorAction, string textAction)
+	{
+		var action = (int)vectorAction[0];
+		Debug.Log("Action : " + action);
+		AddReward(bandit.PullArm(action));
+		Done();
+	}
+
+	public override void AgentReset()
+	{
+		bandit.Reset();
+	}
+
+	public override void CollectObservations()
+    {
+		AddVectorObs(0);
+    }
+	
+   
+    public override void AgentOnDone()
+    {
+
+    }
+
+	public Academy academy;
+	public float timeBetweenDecisionsAtInference;
+	private float timeSinceDecision;
+
+	public void FixedUpdate()
+	{
+		WaitTimeInference();
+	}
+
+	private void WaitTimeInference()
+	{
+		if (!academy.GetIsInference())
+		{
+			RequestDecision();
+		}
+		else
+		{
+			if (timeSinceDecision >= timeBetweenDecisionsAtInference)
+			{
+				timeSinceDecision = 0f;
+				RequestDecision();
+			}
+			else
+			{
+				timeSinceDecision += Time.fixedDeltaTime;
+			}
+		}
+	}
+}
